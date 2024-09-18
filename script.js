87ae96a35b0a57334d2b2a1217e4a74834518c2a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-18 17:21:09 +0530

    Almost done this project

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 
 
+// task adding
 function addTask() {
     const newTask = document.createElement('li');
     const taskList = document.getElementById('taskList');
     taskList.appendChild(newTask);
+
+    newTask.textContent = document.getElementById('taskInput').value;
+    document.getElementById('taskInput').value = '';
+
+    // delete old tasks
+    deleteTask(newTask);
+
+}
+
+
+function deleteTask(newTask) {
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    newTask.appendChild(deleteButton);
+    deleteButton.addEventListener('click', () => {
+        newTask.parentNode.removeChild(newTask);
+    });
 }
